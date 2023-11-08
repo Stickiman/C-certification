6d24893fd4894ade0a909faba33004fa2cacbd2b
Ilya Steputenko <Stickimaniya@gmail.com>
2023-11-08 16:01:49 +0700

    add folders for tasks and complete task 66,68

---
+++ Task 66/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+
+int M = 15;
+int N = 1;
+
+// System.Console.WriteLine("Введите значение числа N");  // Ввод числа N с консоли
+// int N = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите значение числа M");  // Ввод числа M с консоли
+// int M = Convert.ToInt32(Console.ReadLine());
+
+int GetSumNatural(int start, int end)
+{
+    if (start > 0 && end > 0)
+    {
+        if (start == end)
+        {
+            return start;
+        }
+        if (start > end)
+        {
+            System.Console.WriteLine("В связи с тем, что N < M промежуток стал от N до M");
+            return end + GetSumNatural(end + 1, start);
+        }
+        else
+        {
+            return start + GetSumNatural(start + 1, end);
+        }
+    }
+    else
+    {
+        System.Console.WriteLine("Отрицательные числа а также ноль не являются натуральными.");
+        System.Console.WriteLine($"M={M} N={N}");
+        return 0;
+    }
+}
+
+System.Console.WriteLine(GetSumNatural(M,N));
---
+++ Task 68/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана
+//  с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int AKK(int num1,int num2) {
+    if (num1 >= 0 && num2 >= 0)
+    {
+        if (num1 == 0)
+        {
+            return num2 + 1;
+        }
+        else if (num2 == 0)
+        {
+            return AKK(num1 - 1, 1);
+        }
+        else
+        {
+            return AKK(num1 - 1, AKK(num1, num2 - 1));
+        }
+    }
+    else
+    {
+        System.Console.WriteLine("Требуются положительные значения, либо 0");
+        return 0;
+    }
+}
+int m = 2;
+int n = 3;
+
+System.Console.WriteLine(AKK(m,n));
